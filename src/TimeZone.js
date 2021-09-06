25434a0d5ee19e15b52927a57c768744ee211486
Erik <elaktrato@gmail.com>
2021-09-06 21:33:36 +0200

    make timezone throw an error

--- src/TimeZone.js
+++ src/TimeZone.js
@@ -3,19 +3,34 @@ import { DateTime } from "luxon"
 
 function TimeZone(props) {
     let locale = DateTime.local().zoneName;
-    const [currentTime, setCurrentTime] = useState("")
-    
+    const [currentTime, setCurrentTime] = useState("");
+    const [loadingTime, setLoadingTime] = useState(true);
+    const [timeError, setTimeError] = useState(false)
 
     useEffect(() => {
         const interval = setInterval(() => {
-            let timeRightNow = DateTime.now().toFormat('tttt')
-           setCurrentTime(timeRightNow)
+            let timeRightNow = () => {throw error} // DateTime.now().toFormat('tttt')
+            if(timeRightNow !== "" && timeRightNow !== null){
+              setCurrentTime(timeRightNow)
+              setLoadingTime(false)
+            } else if(timeRightNow === null || !timeRightNow){
+              setTimeError(true);
+            }
         }, 1000);
         return () => clearInterval(interval);
-      }, []);
+      }, [currentTime]);
+
+    let timeComponent;
+    if(!loadingTime){
+      timeComponent = `Your time is ${currentTime} ${locale}`
+    } else if(timeError){
+      timeComponent = "Something has gone wrong when trying to get your time! Please try reloading the page"
+    } else {
+      timeComponent = "Loading your time..."
+    }
 
 
-    return <div > Your time is { currentTime } {locale} < /div>
+    return <div> {timeComponent} </div>
 }
 
 export default TimeZone;
