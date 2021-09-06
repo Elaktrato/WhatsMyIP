ea6c33b03434d16c4d530d9f7b7ee87854414b30
Radu Baltag <baltag.radu@gmail.com>
2021-09-06 21:03:36 +0200

    Added React Leaflet Map

--- src/Card.js
+++ src/Card.js
@@ -8,33 +8,35 @@ class Card extends React.Component {
     }
 
     render() {
-
         return (
-        <div className="CardContent">
+            <div className="CardContent">
+                <div id="closeContainer">
+                    <span id="closeBtn"
+                        onClick={() => {
+                            this.props.closeCard();
+                        }}>X</span>
+                </div>
+                <div className="row1">
+                    <div className="FlagContent">
+                        {/* <Flag /> */}
+                    </div>
+                    <div className="infobox">
+                        <div className="IpContent">
+                            <IpDisplay ip={this.props.locationData.ip} /></div>
+                        <div className="TimeZone"><TimeZone /></div>
+                        <div className="BasicInfo">{/* <BasicInfo /> */}</div>
+                    </div>
+                </div>
 
-            <div className="row1">
-            <div className="FlagContent">
-             {/* <Flag /> */}
+                <div className="row2">
+                    <div className="CountryInfo">
+                        <h3>Country Info</h3>
+                        {/* <ExpandedInfo /> */}
+                    </div>
+                </div>
             </div>
-            <div className="infobox">
-            <div className="IpContent">
-                <IpDisplay ip={ this.props.locationData.ip } /></div>
-            <div className="TimeZone"><TimeZone /></div>
-            <div className="BasicInfo">{/* <BasicInfo /> */}</div>
-            </div>
-            </div>
-
-            <div className="row2">
-            <div className="CountryInfo">
-                <h3>Country Info</h3>
-                {/* <ExpandedInfo /> */}
-            </div>
-    </div>
-
-</div>
-
-    )}
-
+        )
+    }
 }
 
 export default Card
