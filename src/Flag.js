4d559874bf90c5ab0e8ec215c502924cf68e622d
Radu Baltag <baltag.radu@gmail.com>
2021-09-08 21:36:29 +0200

    Fixed Flag styling

--- src/Flag.js
+++ src/Flag.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 function Flag(props) {
-  console.log("props.languages");
-
-  
   return (
     <div>
       <img id="countryFlag" src={props.flag} alt="country flag"></img>
